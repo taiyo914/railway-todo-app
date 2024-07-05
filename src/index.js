bc17c1c65659eb3234f55b5229f913a023cdd7fe
taiyo914 <taiyosuzuki0914@icloud.com>
2024-07-05 22:16:16 +0900

    eslintとprettierを設定し、実行しました。33ファイルを一気にコミットします。

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -11,20 +11,20 @@ import { SignUp } from "../pages/SignUp";
 import { EditList } from "../pages/EditList";
 
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         {auth ? (
           <>
-            <Route path="/" element={<Home/>} />
-            <Route path="/task/new" element={<NewTask/>} />
-            <Route path="/list/new" element={<NewList/>} />
-            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask/>} />
-            <Route path="/lists/:listId/edit" element={<EditList/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/task/new" element={<NewTask />} />
+            <Route path="/list/new" element={<NewList />} />
+            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
+            <Route path="/lists/:listId/edit" element={<EditList />} />
           </>
         ) : (
           <Route path="*" element={<Navigate to="/signin" />} />
@@ -32,5 +32,5 @@ export const Router = () => {
         <Route element={NotFound} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
